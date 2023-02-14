a02113e3229e61f8b8c1eb5dd4bdedad9071ab0a
myungvin <>
2023-02-14 14:10:05 +0900

    4-2

--- Day2.java
+++ Day2.java
@@ -9,11 +9,11 @@
 //char형 변수 ch가 숫자('0'~'9')일 때 true인 조건식
   if(ch >='0'&&ch<='9')
 //char형 변수 ch가 영문자(대문자 또는 소문자)일 때 true인 조건식
-if(('A' <= ch && ch <= 'Z'||'a' <= ch && ch <= 'z'))
+  if(('A' <= ch && ch <= 'Z'||'a' <= ch && ch <= 'z'))
 //int형 변수 year가 400으로 나눠떨어지거나 또는 4로 나눠떨어지고 100으로 나눠떨어지지 않을때 true인 조건식
-if((year%400==0)||(year%4==0||year%100!=0))
+  if((year%400==0)||(year%4==0||year%100!=0))
 //boolean형 변수 powerOn이 false일 때 true인 조건식
-if(!powerOn)
+  if(!powerOn)
 
 //문자열 참조변수 str이 "yes"일 때 true인 조건식
 
@@ -23,8 +23,11 @@ if(!powerOn)
 class Exercise4_2 {
     public static void main(String[] args) {
         int sum = 0;
-        /*빈 칸*/
-        System.out.println("sum="+sum);
+        for(int i=1; i <= 20; i++)
+            if(i%3!=0 && i%2!=0) {
+                sum+=i;
+            }
+                System.out.println("sum="+sum);
     }
 }
 
